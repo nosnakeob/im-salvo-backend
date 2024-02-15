2d318dbe623810f24d5bd1e48e559435662d0973
snake <827161257@qq.com>
2024-02-15 17:02:44 +0800

    rbatis访问数据库

--- src/auth.rs
+++ src/auth.rs
@@ -1,6 +1,7 @@
 use once_cell::sync::Lazy;
 use permit_micro::loggedin;
-use rocket::Config;
+use rbatis::{crud, RBatis};
+use rocket::{Config, State};
 use rocket::http::Status;
 use rocket::serde::json::Value;
 use rocket::serde::{Deserialize, Serialize};
@@ -12,46 +13,92 @@ static SECRET_KEY: Lazy<String> = Lazy::new(|| {
     secret.as_str().unwrap().to_owned()
 });
 
+
 #[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone)]
 pub struct User {
-    id: Option<String>,
+    id: Option<u32>,
     username: String,
     password: String,
 }
 
+crud!(User{},"users");
+
+
 #[jwt(SECRET_KEY, exp = 120)]
 pub struct UserClaim {
-    id: String,
+    id: u32,
 }
 
-
-#[post("/login", data = "<user>")]
-pub async fn login(mut user: Json<User>) -> Value {
-    println!("{}", SECRET_KEY);
+#[post("/register", data = "<user>")]
+pub async fn register(user: Json<User>, rb: &State<RBatis>) -> (Status, String) {
     println!("{:?}", user);
+    let rb = &**rb;
 
-    // 存数据库更新id
-    if user.id == None {
-        user.id = Some("1".to_string());
+    match User::select_by_column(rb, "username", &user.username).await {
+        Ok(users) => {
+            if !users.is_empty() {
+                return (Status::BadRequest, "username already exists".to_string());
+            }
+        }
+        Err(err) => return (Status::BadRequest, err.to_string()),
     }
 
-    let user_claim = UserClaim {
-        id: user.id.clone().unwrap(),
+
+    match User::insert(rb, &user).await {
+        Ok(data) => println!("{:?}", data),
+        Err(err) => {
+            return (Status::BadRequest, err.to_string());
+        }
     };
 
-    let token = UserClaim::sign(user_claim);
-    // println!("{:#?}", UserClaim::decode(token.clone()));
-    json!({
-        "code": Status::Ok.code,
-        "token": token
-    })
+    println!("{:?}", user);
+
+    (Status::Ok, "success".to_string())
+}
+
+#[post("/login", data = "<login_user>")]
+pub async fn login(login_user: Json<User>, rb: &State<RBatis>) -> Value {
+    let rb = &**rb;
+
+    match User::select_by_column(rb, "username", &login_user.username).await {
+        Ok(users) => {
+            if users.is_empty() {
+                return json!({
+                    "code": Status::BadRequest,
+                    "msg": "username not exists"
+                });
+            }
+
+            let user = users[0].clone();
+
+            if user.password != login_user.password {
+                return json!({
+                    "code": Status::BadRequest,
+                    "msg": "password error"
+                });
+            }
+            let user_claim = UserClaim {
+                id: user.id.unwrap(),
+            };
+            let token = UserClaim::sign(user_claim);
+            json!({
+                "code": Status::Ok,
+                "token": token
+            })
+        }
+        Err(err) => json!({
+                "code": Status::BadRequest,
+                "msg": err.to_string()
+            })
+    }
 }
 
 #[loggedin]
 #[get("/check")]
 pub async fn check() -> Value {
     json!({
-        "code": Status::Ok.code,
+        "code": Status::Ok,
         "msg": "success"
     })
 }
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,49 @@ mod error;
 
 #[macro_use]
 extern crate rocket;
+extern crate rbatis;
 
-use auth::{login, check};
-use error::default_catcher;
+use rbatis::executor::Executor;
+use rbatis::intercept::{Intercept, ResultType};
+use rbatis::{Error, RBatis};
+use rbdc_pg::PgDriver;
+use rbs::Value;
+use auth::{login, check, register};
+use error::{default_catcher, not_authorized};
+use rbatis::rbdc::db::ExecResult;
+use std::sync::Arc;
+
+#[derive(Debug)]
+pub struct ReturningIdPlugin {}
+
+#[async_trait]
+impl Intercept for ReturningIdPlugin {
+    async fn before(
+        &self,
+        _task_id: i64,
+        rb: &dyn Executor,
+        sql: &mut String,
+        args: &mut Vec<Value>,
+        result: ResultType<&mut Result<ExecResult, Error>, &mut Result<Vec<Value>, Error>>,
+    ) -> Result<bool, Error> {
+        if sql.contains("insert into") {
+            let new_sql = format!("{} {}", sql, "returning id");
+
+            if let ResultType::Exec(exec_r) = result {
+                let id = rb.query(&new_sql, args.clone()).await?;
+                let mut exec = ExecResult::default();
+                exec.rows_affected = id.len() as u64;
+                exec.last_insert_id = id.as_array().unwrap().last().unwrap()["id"].clone();
+
+                *exec_r = Ok(exec);
+
+                return Ok(false);
+            }
+        }
+
+        Ok(true)
+    }
+}
 
 #[get("/")]
 async fn index() -> &'static str {
@@ -14,8 +54,15 @@ async fn index() -> &'static str {
 
 
 #[launch]
-fn rocket() -> _ {
+async fn rocket() -> _ {
+    let rb = RBatis::new();
+
+    rb.link(PgDriver {}, "postgres://postgres:135246@localhost:5432/le_rocket").await.unwrap();
+
+    rb.intercepts.insert(0, Arc::new(ReturningIdPlugin {}));
+
     rocket::build()
-        .mount("/", routes![index, login, check])
         .register("/", catchers![default_catcher])
+        .mount("/", routes![index,register, login, check])
+        .manage(rb)
 }
