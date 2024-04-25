dde488a082155a57ea158f856df9faf09a18338f
snake <827161257@qq.com>
2024-04-25 15:55:12 +0800

    增加登录认证测试

---
+++ src/test/auth.rs
@@ -0,0 +1,46 @@
+use rocket::http::{Header, Status};
+use crate::domain::user::User;
+use crate::framework::rocket::resp::Resp;
+use crate::test::get_client;
+
+#[test]
+fn register() {
+    let client = get_client();
+    let resp = client.post("/auth/register")
+        .json(&User::default())
+        .dispatch();
+
+    if resp.status() != Status::Ok {
+        println!("{:#?}", resp.into_json::<Resp>().unwrap());
+    }
+
+    // 首次注册
+    // assert_eq!(resp.status(), Status::Ok);
+    assert_eq!(resp.status(), Status::BadRequest);
+}
+
+
+#[test]
+fn login() {
+    // 已有对应用户
+    let client = get_client();
+    let mut resp = client.post("/auth/login")
+        .json(&User::default())
+        .dispatch();
+
+    // assert_eq!(resp.status(), Status::Ok);
+    let mut rj: Resp = resp.into_json().unwrap();
+    println!("{:#?}", rj);
+
+    let token = rj.data.unwrap().as_object().unwrap()
+        .get("token").unwrap().as_str().unwrap().to_string();
+
+    resp = client.get("/auth/check")
+        .header(Header::new("Authorization", format!("Bearer {}", token)))
+        .dispatch();
+
+    assert_eq!(resp.status(), Status::Ok);
+
+    rj = resp.into_json().unwrap();
+    println!("{:#?}", rj);
+}
