409166886f0315f8fffc707fb05851b7b0d1d737
snake <827161257@qq.com>
2024-05-22 00:05:01 +0800

    简易代理服务器

---
+++ web-proxy/src/main.rs
@@ -0,0 +1,55 @@
+use anyhow::Result;
+use http_body_util::BodyExt;
+use http_body_util::combinators::BoxBody;
+use hyper::{client, Request, Response};
+use hyper::body::{Bytes, Incoming};
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper_util::rt::TokioIo;
+use tokio::net::{TcpListener, TcpStream};
+
+async fn proxy(req: Request<Incoming>) -> Result<Response<BoxBody<Bytes, hyper::Error>>> {
+    // println!("{:#?}", req);
+    // println!("{}", req.method());
+    // req.version()
+
+    let stream = TcpStream::connect("localhost:8000").await?;
+    let io = TokioIo::new(stream);
+
+    let (mut sender, conn) = client::conn::http1::Builder::new()
+        .preserve_header_case(true)
+        .title_case_headers(true)
+        .handshake(io)
+        .await?;
+
+    tokio::spawn(async move {
+        if let Err(err) = conn.await {
+            println!("Connection failed: {:?}", err);
+        }
+    });
+
+    let resp = sender.send_request(req).await?;
+    Ok(resp.map(|b| b.boxed()))
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let listener = TcpListener::bind("localhost:80").await?;
+
+    println!("Listening on: http://localhost:80");
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        // println!("{:?}", addr);
+
+        tokio::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(TokioIo::new(stream), service_fn(proxy))
+                .await
+            {
+                println!("Failed to serve connection: {:?}", err);
+            }
+        });
+    }
+}
