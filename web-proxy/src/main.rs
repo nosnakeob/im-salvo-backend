8ef08499719ed212dc505ea0e1f90c361e2ba22f
snake <827161257@qq.com>
2024-05-22 15:50:09 +0800

    优雅关机

--- web-proxy/src/main.rs
+++ web-proxy/src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate tokio;
+
 use anyhow::Result;
 use http_body_util::BodyExt;
 use http_body_util::combinators::BoxBody;
@@ -38,18 +41,28 @@ async fn main() -> Result<()> {
 
     println!("Listening on: http://localhost:80");
 
-    loop {
-        let (stream, _) = listener.accept().await?;
+    select! {
+        _ = async {
+            loop {
+                let (stream, _) = listener.accept().await.unwrap();
 
-        // println!("{:?}", addr);
+                // println!("{:?}", addr);
 
-        tokio::spawn(async move {
-            if let Err(err) = http1::Builder::new()
-                .serve_connection(TokioIo::new(stream), service_fn(proxy))
-                .await
-            {
-                println!("Failed to serve connection: {:?}", err);
+                tokio::spawn(async move {
+                    if let Err(err) = http1::Builder::new()
+                        .serve_connection(TokioIo::new(stream), service_fn(proxy))
+                        .await
+                    {
+                        println!("Failed to serve connection: {:?}", err);
+                    }
+                });
             }
-        });
+        } => {}
+
+        _ = tokio::signal::ctrl_c() => {
+            println!("Shutting down");
+        }
     }
+
+    Ok(())
 }
