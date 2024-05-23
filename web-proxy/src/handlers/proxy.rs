f4b6458ec5b20604f3d228d9619d11aabeccb56c
snake <827161257@qq.com>
2024-05-23 22:08:13 +0800

    访问上游服务器失败返回502

--- web-proxy/src/handlers/proxy.rs
+++ web-proxy/src/handlers/proxy.rs
@@ -1,16 +1,29 @@
 use http_body_util::BodyExt;
 use http_body_util::combinators::BoxBody;
 use hyper::body::{Bytes, Incoming};
-use hyper::{client, Request, Response};
+use hyper::{client, Request, Response, StatusCode};
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpStream;
+
+fn bad_gateway() -> Response<BoxBody<Bytes, anyhow::Error>> {
+    Response::builder()
+        .status(StatusCode::BAD_GATEWAY)
+        .body(Full::new("Bad Gateway".into()).map_err(anyhow::Error::from).boxed())
+        .unwrap()
+}
 
 pub async fn proxy(req: Request<Incoming>) -> anyhow::Result<Response<BoxBody<Bytes, anyhow::Error>>> {
     // println!("{:#?}", req);
     // println!("{}", req.method());
     // req.version()
 
-    let stream = TcpStream::connect("localhost:8000").await?;
+    let stream = match TcpStream::connect("localhost:8000").await {
+        Ok(stream) => { stream }
+        Err(_) => {
+            eprintln!("ERROR: Unable to connect to upstream server.");
+            return Ok(bad_gateway());
+        }
+    };
     let io = TokioIo::new(stream);
 
     let (mut upstream_sender, conn) = client::conn::http1::Builder::new()
