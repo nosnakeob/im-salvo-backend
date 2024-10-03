55b8cdbdafeed336e907b9d74b951bf7c6378e74
snake <827161257@qq.com>
2024-10-03 20:01:52 +0800

    增加请求参数认证token

--- src/domain/user.rs
+++ src/domain/user.rs
@@ -3,9 +3,9 @@ use redis::AsyncCommands;
 use redis_macros::{FromRedisValue, ToRedisArgs};
 
 use rocket::http::Status;
-use rocket::{Request, State};
 use rocket::request::{FromRequest, Outcome};
 use rocket::serde::{Deserialize, Serialize};
+use rocket::{Request, State};
 use utoipa::ToSchema;
 
 use web_common::core::constant::cache::token2key;
@@ -21,7 +21,7 @@ pub struct User {
 impl Default for User {
     fn default() -> Self {
         Self {
-            id: Some(123332),
+            id: Some(1),
             username: "snake".to_string(),
             password: "123123".to_string(),
         }
@@ -34,15 +34,26 @@ impl<'r> FromRequest<'r> for User {
     type Error = ();
 
     async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
-        let auth = req.headers().get_one("Authorization").unwrap();
-
-        let token = auth.strip_prefix("Bearer ").unwrap();
-
         let redis_pool: &State<Pool> = req.guard().await.unwrap();
+        let mut conn = redis_pool.get().await.unwrap();
 
-        if let Ok(user) = redis_pool.get().await.unwrap().get(token2key(token)).await {
-            return Outcome::Success(user);
-        };
+        // header Authorization: Bearer token
+        if let Some(token) = req.headers().get_one("Authorization")
+            .and_then(|auth| auth.strip_prefix("Bearer "))
+            .map(|token| token.to_string())
+            // query ?Authorization=Bearer token
+            .or(req.uri().query().and_then(|query|
+                query.split('&')
+                    .filter_map(|s| s.strip_prefix("Authorization=Bearer%20"))
+                    .map(|s| s.to_string())
+                    .last()
+            ))
+        {
+            if let Ok(user) = conn.get(token2key(token)).await {
+                return Outcome::Success(user);
+            };
+        }
+
 
         Outcome::Error((Status::Unauthorized, ()))
     }
