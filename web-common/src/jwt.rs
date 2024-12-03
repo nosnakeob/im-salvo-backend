44406cff78e29abd17e3c099c831d33f29b47e22
snake <827161257@qq.com>
2024-12-03 10:15:14 +0800

    使用rust1.8懒加载

--- web-common/src/jwt.rs
+++ web-common/src/jwt.rs
@@ -1,12 +1,12 @@
 use derive_new::new;
-use once_cell::unsync::Lazy;
 use rocket::figment::value::Value;
 use rocket::Config;
 use rocket_jwt::jwt;
+use std::cell::LazyCell;
 
 use crate::core::utils::config::get_config;
 
-const SECRET_KEY: Lazy<String> = Lazy::new(|| {
+const SECRET_KEY: LazyCell<String> = LazyCell::new(|| {
     get_config(Config::SECRET_KEY).ok()
         .and_then(Value::into_string)
         .unwrap_or("secret".to_string())
